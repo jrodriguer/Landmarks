c5a0a759d8cfe15bdf08ee3e1ba4e8a4d976cd01
Julio <juliorodriguezramirez@gmail.com>
2024-01-02 18:31:45 +0100

    Overlay to give the image a border

--- Landmarks/CircleImage.swift
+++ Landmarks/CircleImage.swift
@@ -10,7 +10,10 @@ import SwiftUI
 struct CircleImage: View {
     var body: some View {
         Image("turtlerock")
-                    .clipShape(Circle())
+            .clipShape(Circle())
+            .overlay {
+                Circle().stroke(.gray, lineWidth: 4)
+            }
     }
 }
 

d2eab9be0669906de75a8eefba2d24650811ad7d
Julio <juliorodriguezramirez@gmail.com>
2024-01-03 17:37:03 +0100

    Navigation among list and detail views

--- Landmarks/Views/LandmarkList.swift
+++ Landmarks/Views/LandmarkList.swift
@@ -11,8 +11,13 @@ struct LandmarkList: View {
     var body: some View {
         NavigationSplitView {
             List(landmarks) { landmark in
-                LandmarkRow(landmark: landmark)
+                NavigationLink {
+                    LandmarkDetail()
+                } label: {
+                    LandmarkRow(landmark: landmark)
+                }
             }
+            .navigationTitle("Landmarks")
         } detail: {
             Text("Select a Landmark")
         }

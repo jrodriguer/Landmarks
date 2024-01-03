9c5b38e3c51b588ee7bec2d580d1afd19e5492ce
Julio <juliorodriguezramirez@gmail.com>
2024-01-03 10:12:00 +0100

    Embed the VStack in another VStack, add Map view for main content

--- Landmarks/ContentView.swift
+++ Landmarks/ContentView.swift
@@ -9,18 +9,22 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack (alignment: .leading) {
-            Text("Turtle Rock")
-                .font(.title)
-            HStack {
-                Text("Joshua Tree National Park")
-                    .font(.subheadline)
-                Spacer()
-                Text("California")
-                    .font(.subheadline)
+        VStack {
+            MapView()
+                .frame(height: 300)
+            VStack (alignment: .leading) {
+                Text("Turtle Rock")
+                    .font(.title)
+                HStack {
+                    Text("Joshua Tree National Park")
+                        .font(.subheadline)
+                    Spacer()
+                    Text("California")
+                        .font(.subheadline)
+                }
             }
+            .padding()
         }
-        .padding()
     }
 }
 

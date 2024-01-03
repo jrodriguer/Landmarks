ce25de4295a5533a17d0cf2be2fbf30543d0b5e5
Julio <juliorodriguezramirez@gmail.com>
2024-01-03 14:46:07 +0100

    Add comments

--- Landmarks/Views/LandmarkList.swift
+++ Landmarks/Views/LandmarkList.swift
@@ -10,6 +10,8 @@ import SwiftUI
 struct LandmarkList: View {
     var body: some View {
         List(landmarks, id: \.id) { landmark in
+            // Data identifiable.
+            // Key path to a property that uniquely identifies each element.
             LandmarkRow(landmark: landmark)
         }
     }

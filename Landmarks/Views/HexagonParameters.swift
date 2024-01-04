9d8064fbb8d2ee9a45f15d5d59e53583deb8cd2b
Julio <juliorodriguezramirez@gmail.com>
2024-01-04 17:38:50 +0100

    Define a Segment structure

---
+++ Landmarks/Views/HexagonParameters.swift
@@ -0,0 +1,17 @@
+//
+//  HexagonParameters.swift
+//  Landmarks
+//
+//  Created by Julio Rodriguez on 4/1/24.
+//
+
+import Foundation
+import CoreGraphics
+
+struct HexagonParameters {
+    struct Segment {
+        let line: CGPoint
+        let curve: CGPoint
+        let control: CGPoint
+    }
+}

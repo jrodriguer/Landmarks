81d8322087708ebecf9380edaf8ac603d82cc7de
Julio <juliorodriguezramirez@gmail.com>
2024-01-05 13:13:00 +0100

    Add a starting point to the path (100x100) and draw the lines for each point

--- Landmarks/Views/BadgeBackground.swift
+++ Landmarks/Views/BadgeBackground.swift
@@ -5,4 +5,33 @@
 //  Created by Julio Rodriguez on 4/1/24.
 //
 
-import Foundation
+import SwiftUI
+
+struct BadgeBackground: View {
+    var body: some View {
+        Path { path in
+            var width: CGFloat = 100.0
+            let height = width
+            path.move(
+                to: CGPoint(
+                    x: width * 0.95,
+                    y: height * 0.20
+                )
+            )
+            
+            HexagonParameters.segments.forEach { segment in
+                path.addLine(
+                    to: CGPoint(
+                        x: width * segment.line.x,
+                        y: height * segment.line.y
+                    )
+                )
+            }
+        }
+        .fill(.black)
+    }
+}
+
+#Preview {
+    BadgeBackground()
+}

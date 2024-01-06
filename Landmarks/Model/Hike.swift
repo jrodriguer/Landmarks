3af50e52da53d80121619f4d2204e8fca4668be7
Julio <juliorodriguezramirez@gmail.com>
2024-01-06 17:50:38 +0100

    Like the Landmark structure

--- Landmarks/Model/Hike.swift
+++ Landmarks/Model/Hike.swift
@@ -6,3 +6,30 @@
 //
 
 import Foundation
+import SwiftUI
+
+struct Hike: Hashable, Codable, Identifiable {
+    var id: Int
+    var name: String
+    var distance: Double
+    var difficulty: String
+    var observations: [Observation]
+    
+    static var formatter = LengthFormatter()
+
+
+        var distanceText: String {
+            Hike.formatter
+                .string(fromValue: distance, unit: .kilometer)
+        }
+
+
+        struct Observation: Codable, Hashable {
+            var distanceFromStart: Double
+
+
+            var elevation: Range<Double>
+            var pace: Range<Double>
+            var heartRate: Range<Double>
+        }
+}

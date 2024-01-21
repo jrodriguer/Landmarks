1a86c357fce2b09da9688da4cb2aad602d401dc5
Julio <juliorodriguezramirez@gmail.com>
2024-01-21 07:42:52 +0100

    Computed property to the Landmark structure that returns the feature image

--- Landmarks/Views/PageView/PageViewController.swift
+++ Landmarks/Views/PageView/PageViewController.swift
@@ -6,13 +6,27 @@
 //
 
 import SwiftUI
+import UIKit
 
-struct PageViewController: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+/// The page view controller stores an array of Page instances, which must be a type of View.
+/// These are the pages you use to scroll between landmarks.
+struct PageViewController<Page: View>: UIViewControllerRepresentable {
+    var pages: [Page]
+    
+    ///
+    /// Method that creates a UIPageViewController with the desired configuration.
+    func makeUIViewController(context: Context) -> UIPageViewController {
+        let pageViewController = UIPageViewController(
+            transitionStyle: .scroll,
+            navigationOrientation: .horizontal)
+        
+        return pageViewController
     }
-}
-
-#Preview {
-    PageViewController()
+    
+    ///
+    /// setViewControllers(_:direction:animated:) to provide a view controller for display.
+    func updateUIViewController(_ pageViewController: UIPageViewController, context: Context) {
+            pageViewController.setViewControllers(
+                [UIHostingController(rootView: pages[0])], direction: .forward, animated: true)
+        }
 }

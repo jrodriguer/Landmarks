431acf96e73c5ed1fdbc4ae7b78d0b6cf3cd5a91
Julio <juliorodriguezramirez@gmail.com>
2024-01-22 13:08:03 +0100

    Add the aspect ratio modifier and update the preview to pass the required array

--- Landmarks/Views/PageView/PageView.swift
+++ Landmarks/Views/PageView/PageView.swift
@@ -12,9 +12,10 @@ struct PageView: View {
 
     var body: some View {
         PageViewController(pages: pages)
+            .aspectRatio(3 / 2, contentMode: .fit)
     }
 }
 
 #Preview {
-    PageView()
+    PageView(pages: ModelData().features.map { FeatureCard(landmark: $0) })
 }

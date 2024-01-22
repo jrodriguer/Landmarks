f9551bf4120cb79d4047cafe5659326ef5510414
Julio <juliorodriguezramirez@gmail.com>
2024-01-22 13:06:50 +0100

    To present UIViewControllerRepresentable view

--- Landmarks/Views/PageView/PageView.swift
+++ Landmarks/Views/PageView/PageView.swift
@@ -8,8 +8,10 @@
 import SwiftUI
 
 struct PageView: View {
+    var pages: [Page]
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        PageViewController(pages: pages)
     }
 }
 
